716f912f279d1c8e6bc84eb2a32e0dbb7c38f27f
maxim <skrupulezno@gmail.com>
2024-07-23 03:44:51 +0900

    init

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  setCount(count + 1);
+  return <>{count}</>;
+}
+
+export default App;
